5287d590805a6ffc83edd2fa4c17bf54178eb758
Michelle Perez Morales <michelle.perez@sngular.com>
2023-05-16 21:55:47 -0400

    stable version first view

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: any) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
